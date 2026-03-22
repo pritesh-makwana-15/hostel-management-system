186706aa3881b1a92fd5ea0344bb2d2c193ddeb1
pritesh makwana <priteshrm15@gmail.com>
2026-03-22 17:04:59 +0530

    warden student pages

--- frontend/src/pages/warden/Students.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ComingSoon from '../../components/common/ComingSoon';
-
-const Students = () => {
-  return <ComingSoon title="Students Management" />;
-};
-
-export default Students;
